64f653eb06dd54e0b848d2576503a51a65a5962e
Olmagzar <olmagzar@gmail.com>
2021-09-29 12:42:16 +0200

    Add map generation with several players
    Signed-off-by: Olmagzar <olmagzar@gmail.com>

--- drawMap.py
+++ drawMap.py
@@ -3,30 +3,22 @@
 import math
 from PIL import Image, ImageDraw
 
-"""h = 1900
-w = 1900
-L = 5
-"""
-h = 500
-w = 500
-L = 10
-rx = 2
-ry = 2
+h = 4096
+w = 4096
+L = 7
 
-radius = 6 + 1
-
-ville = (0, 0, 0)
 space = (20, 30, 40)
-marbre = (250, 235, 215)
-planches = (205, 127, 50)
-acier = (111, 127, 128)
-cristal = (0, 255, 255)
-parcho = (233, 214, 107)
-soie = (237, 135, 45)
-elixir = (224, 60, 49)
-poussiere = (255, 166, 201)
-gemmes = (189, 51, 164)
-cl = 1/2
+space = 'black'
+
+stellar_color = [
+        (0xff, 0x45, 0x00),
+        (0xff, 0xae, 0x42),
+        (0xef, 0xe4, 0x9a),
+        (0xd8, 0xd7, 0xdf),
+        (0x9a, 0xd7, 0xff),
+        (0x4a, 0x8f, 0xdf),
+        (0x0b, 0x50, 0xaf),
+        ]
 
 class HexagonGenerator(object):
     """Returns a hexagon generator for hexagons of the specified size."""
@@ -50,25 +42,77 @@ class HexagonGenerator(object):
             yield x
             yield y
 
-def getColor(city_r):
+def getCategory(city_r):
     solar_r = 6
     solar_c = 2
     radius_per_c = (solar_r / solar_c)
-    stellar_color = [
-            (0xff, 0x45, 0x00),
-            (0xff, 0xae, 0x42),
-            (0xef, 0xe4, 0x9a),
-            (0xd8, 0xd7, 0xdf),
-            (0x7a, 0xb7, 0xef),
-            (0x5a, 0x9f, 0xef),
-            (0x2b, 0x70, 0xcf),
-            ]
     category = math.floor(city_r / radius_per_c)
     index = min(category, len(stellar_color) - 1)
+    return index
+
+def getColor(city_r):
+    index = getCategory(city_r)
     return stellar_color[index]
 
-offset_x = 0
-offset_y = 0
+def addCity(offset_x, offset_y, radius, draw, hexagon_generator, draw_aura = True):
+    offset_y = offset_y - math.ceil(offset_x / 2)
+    rad_aura = radius
+    if (not draw_aura):
+        radius = 1
+    radius += 1
+    for row in range(-radius, radius):
+        row += offset_y
+        for col in range(-radius, radius):
+            col += offset_x
+
+            local_r = int((abs(col - offset_x) + abs(row - offset_y) + abs(col + row - offset_x - offset_y)) / 2)
+            if (local_r < radius):
+                m_r = radius - local_r
+                color = getColor(rad_aura)
+                color = (color[0] + 10 * m_r,
+                         color[1] + 10 * m_r,
+                         color[2] + 10 * m_r)
+
+                hexagon = hexagon_generator(row, col)
+                draw.polygon(list(hexagon), color)
+
+conf = [
+        { "name": "JMT", "x": -113, "y": -68, "scout": 49459 },
+        { "name": "Rose", "x": -73, "y": -44, "scout": 48830 },
+        { "name": "Letraz", "x": 9, "y": -77, "scout": 54202 },
+        { "name": "Micha", "x": 25, "y": 7, "scout": 47408 },
+        { "name": "Laplus", "x": -97, "y": -65, "scout": 38550 },
+        { "name": "Salvi", "x": -49, "y": 118, "scout": 44744 },
+        { "name": "gradlon", "x": 78, "y": 72, "scout": 40112 },
+        { "name": "Dura", "x": 135, "y": -80, "scout": 34031 },
+        { "name": "As", "x": 155, "y": 34, "scout": 25489 },
+        { "name": "Ares", "x": 79, "y": 19, "scout": 21797 },
+        { "name": "wisch", "x": -92, "y": 36, "scout": 32223 },
+        { "name": "ram", "x": -23, "y": -107, "scout": 21313 },
+        { "name": "wizard", "x": 83, "y": 28, "scout": 26954 },
+        { "name": "Fari", "x": 55, "y": -38, "scout": 21636 },
+        { "name": "moar", "x": -112, "y": -30, "scout": 27294 },
+        { "name": "Zeus", "x": 94, "y": 72, "scout": 15769 },
+        { "name": "Julia", "x": 62, "y": -114, "scout": 18056 },
+        { "name": "poupou", "x": -35, "y": -2, "scout": 6569 },
+        { "name": "Cirederf", "x": -8, "y": 39, "scout": 6836 },
+        { "name": "Jabal", "x": 111, "y": -89, "scout": 7328 },
+        { "name": "Uruk", "x": -119, "y": 64, "scout": 5795 },
+        { "name": "Zoubou", "x": 79, "y": -17, "scout": 64 }
+        ]
+
+def tiles(n):
+    entier = math.floor(n/3)
+    extra = 0
+    for k in range(3 * entier, n + 1):
+        extra += (k * math.floor( (2*k + 1) / 3 ))
+    return (3 * entier * ( (entier - 1) * (2 * entier + 1) + 1) + extra)
+
+def getRadius(points):
+    n = math.floor((points/48) ** (1/3))
+    while (48 * tiles(n)) < points:
+        n += 1
+    return (n - 1)
 
 def main():
     image = Image.new('RGB', (w, h), space)
@@ -76,60 +120,13 @@ def main():
     hexagon_generator = HexagonGenerator(L)
     max_row = math.ceil(h / L)
     max_col = math.ceil(w / (L * 3/2))
-    for row in range(-round(max_row/2), round(max_row/2)):
-        for col in range(-round(max_col/2), round(max_col/2)):
+    for player in conf:
+        player_r = getRadius(player["scout"])
+        """print(player["name"], "radius:", player_r, ", category:", getCategory(player_r))"""
+        addCity(player["x"], player["y"], getRadius(player["scout"]), draw, hexagon_generator)
 
-            local_r = int((abs(col - offset_x) + abs(row - offset_y) + abs(col + row - offset_x - offset_y)) / 2) 
-            if (local_r < radius):
-                m_r = radius - local_r
-                color = getColor(radius - 1)
-                color = (color[0] + 10 * m_r,
-                         color[1] + 10 * m_r,
-                         color[2] + 10 * m_r)
+    """draw.polygon(list(hexagon_generator(0, 0)), 'green')"""
 
-                hexagon = hexagon_generator(row, col)
-                draw.polygon(list(hexagon), color)
-            """
-            if (abs(col - offset_x) < radius and abs(col - offset_x + row - offset_y) < radius and abs(row - offset_y) < radius):
-            elif ( abs(row - col) % 3 == 0 ):
-                color = ville
-            else:
-                color = space
-
-            elif ( col % 3 == 0 and (((row - 4) % 9) % 4) == 0):
-                color = marbre
-            elif ( col % 3 == 0 and (((row - 1) % 9) % 4) == 0):
-                color = planches
-            elif ( col % 3 == 0 and (((row + 2) % 9) % 4) == 0):
-                color = acier
-
-            elif ( (col - 1)% 3 == 0 and (((row + 4) % 9) % 4) == 0):
-                color = cristal
-            elif ( (col - 1) % 3 == 0 and (((row + 1) % 9) % 4) == 0):
-                color = parcho
-            elif ( (col - 1)% 3 == 0 and (((row - 2) % 9) % 4) == 0):
-                color = soie
-
-            elif ( (col - 2) % 3 == 0 and (((row + 3) % 9) % 4) == 0):
-                color = elixir
-            elif ( (col - 2)% 3 == 0 and (((row) % 9) % 4) == 0):
-                color = poussiere
-            elif ( (col - 2)% 3 == 0 and (((row - 3) % 9) % 4) == 0):
-                color = gemmes
-            """
-
-
-    """
-    hexagon = hexagon_generator(0, 0)
-    draw.polygon(list(hexagon), 'green')
-    hexagon = hexagon_generator(0, 1)
-    draw.polygon(list(hexagon), 'green')
-    hexagon = hexagon_generator(0, 2)
-    draw.polygon(list(hexagon), 'blue')
-
-    hexagon = hexagon_generator(1, 0)
-    draw.polygon(list(hexagon), 'yellow')
-    """
     image.show()
 
 
