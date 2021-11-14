5832bb60b47de635a7cc9fdc922ed547849f3060
Olmagzar <olmagzar@gmail.com>
2021-11-15 00:33:00 +0100

    Refactor geometry details into a GameGeom class
    Add ColorSet class and specific subclasses which define the getColorIdx
    method to retrieve the index of the color defined in the given subclass.
    Add argument parser to allow disabling creating the map of all players
    and/or creating the map of activity status of the cities.
    Use the file specified as database containing details of the players.
    
    Stellar colorset class defines seven colors assigned according to the
    estimated exploration area computed from the encounter score:
     - red: for cities which explored two full rings or less
     - orange: for cities which explored five full rings or less
     - yellow: for cities which explored eight full rings or less
     - white: for cities which explored eleven full rings or less
     - light blue: for cities which explored fourteen full rings or less
     - blue: for cities which explored seventeen full rings or less
     - dark blue: for cities which explored eighteen full rings or more
    These colors were choosen to follow the stars color from red, the small
    and cold ones, to blue the large and hot ones. It is not the exact
    colorset of stars because the colors are too light and alike to be
    correctly interpreted on the map. Adjustments were required.
    This choice of colorset was inspired by a fellow member who noticed that
    the first attempt to display the location of the cities in a graph
    looked like a constellation.
    
    Forest colorset class defines six colors assigned according to the
    estimated period of inactivity of each city (some might be active but
    not detected):
     - green: for cities active in the last six days
     - light green: for cities active in the last thirteen days
     - yellow: for cities active in the last tweenty days
     - red: for cities active in the last tweenty-six days
     - brown: for cities active in the last thirty-four days
     - black: for cities inactive since thirty-four days or more
    These colors were chosen to give inactive cities a look of trees in the
    fall or in the winter and to give active cities a look of trees in the
    spring or in the summer.
    
    usage: drawMap.py [-h] [--all | --no-all] [--actives | --no-actives]
                      filename
    
    Signed-off-by: Olmagzar <olmagzar@gmail.com>

--- config.py
+++ config.py
@@ -1,7 +1,84 @@
-from fellowships import ptiRosours, deuxCouronnes, odyssée
+import math
 
-guildes = [
-    ptiRosours.conf,
-    deuxCouronnes.conf,
-    #odyssée.conf
-]
+fn = 'players.json'
+h = 5500
+w = 5500
+L = 7
+
+prefix_path = './maps/'
+
+class ColorSet():
+    def __init__(self, palette):
+        if self.__class__ is ColorSet:
+            raise Exception("Cannot instanciate from an abstract class")
+        else:
+            self.__background = (0, 0, 0)
+            self.__axis = (92, 92, 92)
+            self.__links = (128, 128, 128)
+            self.__palette = palette
+
+    def __getBackground(self):
+        return self.__background
+    def __getAxis(self):
+        return self.__axis
+    def __getLinks(self):
+        return self.__links
+    def __getPalette(self):
+        return self.__palette
+
+    background = property(__getBackground)
+    axis = property(__getAxis)
+    links = property(__getLinks)
+    palette = property(__getPalette)
+
+    def getColorIdx(self, value):
+        pass
+
+class StellarSet(ColorSet):
+    def __init__(self):
+        self.__stellar_color = [
+            (0xff, 0x45, 0x00),
+            (0xff, 0xae, 0x42),
+            (0xef, 0xe4, 0x9a),
+            (0xd8, 0xd7, 0xdf),
+            (0x9a, 0xd7, 0xff),
+            (0x4a, 0x8f, 0xdf),
+            (0x0b, 0x50, 0xaf),
+        ]
+        super().__init__(self.__stellar_color)
+    def getColorIdx(self, city_r):
+        solar_r = 6
+        solar_c = 2
+        radius_per_c = (solar_r / solar_c)
+        category = math.floor(city_r / radius_per_c)
+        index = min(category, len(self.palette) - 1)
+        return index
+
+class OverlapSet(ColorSet):
+    def __init__(self):
+        self.__overlap_color = [
+            (0, 127, 192),
+            (192, 127, 0)
+        ]
+        super().__init__(self.__overlap_color)
+    def getColorIdx(self, isForeignFS):
+        if isForeignFS == True:
+            index = 1
+        else:
+            index = 0
+        return index
+
+class ForestSet(ColorSet):
+    def __init__(self):
+        self.__forest_color = [
+            (0x00, 0x99, 0x00),
+            (0x99, 0xcc, 0x00),
+            (0xcc, 0x99, 0x00),
+            (0xcc, 0x33, 0x00),
+            (0x66, 0x33, 0x00),
+            (0x00, 0x00, 0x00),
+        ]
+        super().__init__(self.__forest_color)
+    def getColorIdx(self, value):
+        index = int((35 - value)/7)
+        return index
--- drawMap.py
+++ drawMap.py
@@ -1,67 +1,107 @@
 #!/usr/bin/python
 
 import os
+import sys
+import argparse
 import json
 import math
 from PIL import Image, ImageDraw, ImageFont
-#from config import guildes
+import config
 
-fn = 'active_players.json'
-fd = os.open(fn, os.O_RDONLY)
-sz = os.path.getsize(fn)
-buf = os.read(fd, sz)
-os.close(fd)
+def tiles(n):
+    entier = math.floor(n/3)
+    extra = 0
+    for k in range(3 * entier, n + 1):
+        extra += (k * math.floor( (2*k + 1) / 3 ))
+    return (3 * entier * ( (entier - 1) * (2 * entier + 1) + 1) + extra)
 
-guildes = [ { 'name': 'active-players', 'members': json.loads(buf.decode()) } ]
-h = 5500
-w = 5500
-L = 7
+def getRadius(points):
+    n = math.floor((points/48) ** (1/3))
+    while (48 * tiles(n)) < points:
+        n += 1
+    return (n - 1)
 
-prefix_path = './maps/'
-
-space = (0, 0, 0)
-axis = (92, 92, 92) # Or (128, 128, 128)?
-stellar_color = [
-        (0xff, 0x45, 0x00),
-        (0xff, 0xae, 0x42),
-        (0xef, 0xe4, 0x9a),
-        (0xd8, 0xd7, 0xdf),
-        (0x9a, 0xd7, 0xff),
-        (0x4a, 0x8f, 0xdf),
-        (0x0b, 0x50, 0xaf),
-        ]
-
-stellar_set = [
-        space,
-        axis,
-        (128, 128, 128),
-        stellar_color
-        ]
-
-class Map():
-    # TODO: Add colorset parameter
-    def __init__(self, width, height, bg_color, axis_color, edge_length, title):
+# Game geometry: hexagons
+class GameGeom():
+    def __init__(self, width, height, edge_length):
         self.__width = width
         self.__height = height
         self.__edge_length = edge_length
-        self.bg_color = bg_color
-        self.axis_color = axis_color
-        self.title = title
-        self.__image = Image.new('RGB', (self.__width, self.__height), self.bg_color)
+
+    #  _|_._      \|
+    #   |     ->   |\
+    #   |          | \
+    @staticmethod
+    def gameToGeomCoord(row, col):
+        hex_col = col
+        hex_row = row - math.floor(col / 2)
+        return (hex_row, hex_col)
+
+    def __geomCoordToXYpx(self, hex_row, hex_col):
+        hex_h = math.sin(math.pi / 3) * self.__edge_length
+        ox = hex_col * 3/2 * self.__edge_length + self.__width/2
+        oy = (2 * hex_row + hex_col) * hex_h + self.__height/2
+        return (ox, oy)
+
+    def getCenterPx(self, city):
+        (row, col) = self.gameToGeomCoord(row = city["y"], col = city["x"])
+        return self.__hexCoordToXYpx(row, col)
+
+    def tile(self, row, col):
+        (ox, oy) = self.__geomCoordToXYpx(row, col)
+        for angle in range(0, 360, 60):
+            x = math.cos(math.radians(angle)) * self.__edge_length + ox
+            y = math.sin(math.radians(angle)) * self.__edge_length + oy
+            yield x
+            yield y
+
+    @staticmethod
+    def tileDist(row_1, col_1, row_2, col_2):
+        return int((abs(col_2 - col_1) + abs(row_2 - row_1) + abs(col_2 + row_2 - col_1 - row_1)) / 2)
+
+    def cityDist(self, city_1, city_2):
+        (row_1, col_1) = self.gameToGeomCoord(col = city_1["x"], row = city_1["y"])
+        (row_2, col_2) = self.gameToGeomCoord(col = city_2["x"], row = city_2["y"])
+        return self.tileDist(row_1, col_1, row_2, col_2)
+
+    def range(self, center, radius):
+        tile_set = []
+        (offset_y, offset_x) = self.gameToGeomCoord(col = center["x"],
+                                                    row = center["y"])
+        for row in range(-radius, radius):
+            row += offset_y
+            for col in range(-radius, radius):
+                col += offset_x
+                local_r = self.tileDist(row_1 = offset_y, col_1 = offset_x,
+                                        row_2 = row,      col_2 = col)
+                if (local_r < radius):
+                    tile_set.append({ 'm_r': (radius - local_r), 'coords': self.tile(row, col) })
+        return tile_set
+
+class Map():
+    def __init__(self, width, height, edge_length, color_set, title):
+        self.__width = width
+        self.__height = height
+        self.__edge_length = edge_length
+        self.__geom = GameGeom(self.__width, self.__height, self.__edge_length)
+        self.__color_set = color_set
+        self.__title = title
+        self.__image = Image.new('RGB', (self.__width, self.__height),
+                                 self.__color_set.background)
         self.__draw = ImageDraw.Draw(self.__image)
-        self.font = ImageFont.truetype('/usr/share/fonts/droid/DroidSans.ttf', 64)
+        self.__font = ImageFont.truetype('/usr/share/fonts/droid/DroidSans.ttf', 64)
 
     def putAxis(self):
         self.__draw.line((self.__edge_length,
                           self.__height/2,
                           self.__width - self.__edge_length,
                           self.__height/2),
-                         self.axis_color, int(self.__edge_length/2))
+                         self.__color_set.axis, int(self.__edge_length/2))
         self.__draw.line((self.__width/2,
                           self.__edge_length,
                           self.__width/2,
                           self.__height - self.__edge_length),
-                         self.axis_color, int(self.__edge_length/2))
+                         self.__color_set.axis, int(self.__edge_length/2))
 
     def save(self, prefix_path):
         if not os.path.isdir(prefix_path):
@@ -70,88 +110,33 @@ class Map():
             except:
                 print('Unable to create directory', prefix_path)
                 exit(1)
-        self.__image.save(prefix_path + self.title)
+        self.__image.save('{}{}.png'.format(prefix_path, self.__title))
 
     def clear(self):
-        self.__draw((0, 0, self.__width, self.__height), self.bg_color)
+        self.__draw((0, 0, self.__width, self.__height), self.__color_set.background)
 
-    #  _|_._      \|
-    #   |     ->   |\
-    #   |          | \
-    @staticmethod
-    def __gametoHexCoord(row, col):
-        hex_col = col
-        hex_row = row - math.floor(col / 2)
-        return (hex_row, hex_col)
-
-    def __hexCoordToXYpx(self, hex_row, hex_col):
-        hex_h = math.sin(math.pi / 3) * self.__edge_length
-        ox = hex_col * 3/2 * self.__edge_length + self.__width/2
-        oy = (2 * hex_row + hex_col) * hex_h + self.__height/2
-        return (ox, oy)
-
-    def __getCenterPx(self, city):
-        (row, col) = self.__gametoHexCoord(row = city["y"], col = city["x"])
-        return self.__hexCoordToXYpx(row, col)
-
-    def __hexagon(self, row, col):
-        (ox, oy) = self.__hexCoordToXYpx(row, col)
-        for angle in range(0, 360, 60):
-            x = math.cos(math.radians(angle)) * self.__edge_length + ox
-            y = math.sin(math.radians(angle)) * self.__edge_length + oy
-            yield x
-            yield y
-
-    def addCity(self, player, draw_aura = True, use_color = None):
-        (offset_y, offset_x) = self.__gametoHexCoord(col = player["x"], row = player["y"])
-        radius = getRadius(player["encounter"])
-        rad_aura = radius
+    def addCity(self, player, color_idx, draw_aura = True):
         if (not draw_aura):
             radius = 0
+        else:
+            radius = getRadius(player["encounter"])
         radius += 1
-        for row in range(-radius, radius):
-            row += offset_y
-            for col in range(-radius, radius):
-                col += offset_x
 
-                local_r = self.__tileDist(row_1 = offset_y, col_1 = offset_x,
-                                          row_2 = row,      col_2 = col)
-                if (local_r < radius):
-                    m_r = radius - local_r
-                    if not use_color:
-                        # NOTE: should it always be dimmed?
-                        color = getColor(rad_aura)
-                        color = (color[0] + 10 * m_r,
-                                 color[1] + 10 * m_r,
-                                 color[2] + 10 * m_r)
-                    else:
-                        color = use_color
+        for tile in self.__geom.range(player, radius):
+            m_r = tile['m_r'] - 1
+            color = self.__color_set.palette[color_idx]
+            color = (color[0] + 10 * m_r,
+                     color[1] + 10 * m_r,
+                     color[2] + 10 * m_r)
 
-                    old_color = self.__draw.im.getpixel(self.__hexCoordToXYpx(row, col))
-                    # NOTE: overlap part, should it be a specified argument?
-#                    if old_color != space:
-#                        color = (int((color[0] + old_color[0]) / 2),
-#                                 int((color[1] + old_color[1]) / 2),
-#                                 int((color[2] + old_color[2]) / 2))
-#                        print(player["name"], "override color", old_color)
-
-                    self.__draw.polygon(list(self.__hexagon(row, col)), color)
-
-    # NOTE: Maybe should let these into a HexGameGeom(GameGeom) object.
-    def __cityDist(self, city_1, city_2):
-        (row_1, col_1) = self.__gametoHexCoord(col = city_1["x"], row = city_1["y"])
-        (row_2, col_2) = self.__gametoHexCoord(col = city_2["x"], row = city_2["y"])
-        return self.__tileDist(row_1, col_1, row_2, col_2)
-
-    @staticmethod
-    def __tileDist(row_1, col_1, row_2, col_2):
-        return int((abs(col_2 - col_1) + abs(row_2 - row_1) + abs(col_2 + row_2 - col_1 - row_1)) / 2)
+            if color != self.__color_set.background:
+                self.__draw.polygon(list(tile['coords']), color)
 
     def __prim(self, cities):
         sz = len(cities)
         distMatrix = []
         for city in cities:
-            localDist = [ self.__cityDist(city, neighbour)
+            localDist = [ self.__geom.cityDist(city, neighbour)
                           for neighbour in cities ]
             distMatrix.append(localDist)
         # Start of generic prim
@@ -173,81 +158,94 @@ class Map():
                 # TODO: check if this part cannot ignore the geometric details
                 #       to export prim as a generic algorithm and wrap __prim
                 #       around it to apply to class map
-                p1 = self.__getCenterPx(cities[start])
-                p2 = self.__getCenterPx(cities[end])
+                p1 = self.__geom.getCenterPx(cities[start])
+                p2 = self.__geom.getCenterPx(cities[end])
                 resultTree.append(p1 + p2)
         # End of prim
         return resultTree
 
     def addLinks(self, cities):
         for line in self.__prim(cities):
-            self.__draw.line(line, self.axis_color, int(self.__edge_length/2))
+            self.__draw.line(line, self.__color_set.links, int(self.__edge_length/2))
 
-    # TODO: Add name function/option somewhere
-    # TODO: Add legend -> implies color regulation instead of user choice
+    def addNames(self, cities, color_idx):
+        for city in cities:
+            (ox, oy) = self.__geom.getCenterPx(city)
+            txt_sz = self.__draw.textsize(city['name'], self.__font)
+            self.__draw.text((ox - txt_sz[0]/2, oy - 5 * self.__edge_length - txt_sz[1]),
+                             city["name"],
+                             color_idx,
+                             self.__font)
 
-def getCategory(city_r):
-    solar_r = 6
-    solar_c = 2
-    radius_per_c = (solar_r / solar_c)
-    category = math.floor(city_r / radius_per_c)
-    index = min(category, len(stellar_color) - 1)
-    return index
+    # TODO: Add legend
 
-def getColor(city_r):
-    index = getCategory(city_r)
-    return stellar_color[index]
+def main(fn = config.fn, drawAll = True, drawActives = True):
+    fd = os.open(fn, os.O_RDONLY)
+    sz = os.path.getsize(fn)
+    buf = os.read(fd, sz)
+    os.close(fd)
+    data = json.loads(buf.decode())
+    del buf
+    cities = [ data[k] for k in data ]
+    name = os.path.splitext(os.path.basename(fn))[0]
 
-# NOTE: should 'tiles' and getRadius be part of class map as they are
-#       bound to game geometry details?
-def tiles(n):
-    entier = math.floor(n/3)
-    extra = 0
-    for k in range(3 * entier, n + 1):
-        extra += (k * math.floor( (2*k + 1) / 3 ))
-    return (3 * entier * ( (entier - 1) * (2 * entier + 1) + 1) + extra)
+    if drawAll == True:
+        color_set = config.StellarSet()
+        stellar_map = Map(config.w, config.h, config.L, color_set, 'all-' + name)
+        stellar_map.putAxis()
+        for player in cities:
+            color_idx = color_set.getColorIdx(getRadius(player['encounter']))
+            stellar_map.addCity(player, color_idx, draw_aura = False)
+        stellar_map.save(config.prefix_path)
 
-def getRadius(points):
-    n = math.floor((points/48) ** (1/3))
-    while (48 * tiles(n)) < points:
-        n += 1
-    return (n - 1)
+    if drawActives == True:
+        color_set = config.ForestSet()
+        forest_map = Map(config.w, config.h, config.L, color_set, 'active-' + name)
+        forest_map.putAxis()
+        for player in cities:
+            color_idx = color_set.getColorIdx(player['active_period'])
+            forest_map.addCity(player, color_idx, draw_aura = False)
+        forest_map.save(config.prefix_path)
 
-def main():
-    for conf in guildes:
-        my_map = Map(w, h, space, axis, L, '{}.png'.format(conf['name']))
-        my_map.putAxis()
-        # Draw the current fellowship with aura of each members and save it
-        print('Nb of active players: {}'.format(len(conf['members'])))
-        for player in conf["members"]:
-            # TODO: use class map
-            my_map.addCity(player, draw_aura = False)
-        my_map.save(prefix_path)
-#    my_map.clear()
-#
-        # Draw the current fellowship without aura but add name of members
-        # and minimal spanning tree using prim algorithm
+        # 1) Draw the current fellowship with aura of each members and save it
+        # 2) Draw the current fellowship without aura but add name of members
+        #    and minimal spanning tree using prim algorithm
+        # 3) Draw overlap of all fellowship to see trading opportunities
 
-#        for player in conf["members"]:
-#            addCity(player, draw, draw_aura = False)
-#        for player in conf["members"]:
-#            (ox, oy) = getCenterPx(player)
-#            txt_sz = draw.textsize(player["name"], font)
-#            draw.text((ox - txt_sz[0]/2, oy - 5 * L - txt_sz[1]),
-#                      player["name"],
-#                      getColor(getRadius(player["encounter"])),
-#                      font)
-#
 #    if len(guildes) < 2:
 #        exit(0)
-#    draw.line((L, h/2, w - L, h/2), (92, 92, 92), int(L/2))
-#    draw.line((w/2, L, w/2, h - L), (92, 92, 92), int(L/2))
+#    overlap_set = {
+#       'background': (0, 0, 0),
+#       'axis': (92, 92, 92),
+#       'links': (128, 128, 128),
+#       'palette': [
+#           (0, 127, 192),
+#           (192, 127, 0)
+#       ]
+#    }
+#    my_map = Map(config.w, config.h, config.L, overlap_set, 'overlap.png')
+#    my_map.putAxis()
 #    for conf in guildes:
-#        # Draw overlap of all fellowship to see trading opportunities
 #        if conf["name"] == guildes[0]["name"]:
-#            use_color = (0, 127, 192)
+#            color_idx = 0
 #        else:
-#            use_color = (192, 127, 0)
+#            color_idx = 1
 #        for player in conf["members"]:
-#            addCity(player, draw, draw_aura = True, use_color = use_color)
-main()
+#            my_map.addCity(player, color_idx, draw_aura = True)
+#    my_map.save(config.prefix_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Draw maps of players from json database.')
+    parser.add_argument('--all', action=argparse.BooleanOptionalAction,
+                        default=True, type=bool, help='Draw map of all players')
+    parser.add_argument('--actives', action=argparse.BooleanOptionalAction,
+                        default=True, type=bool, help='Draw map of activity')
+    parser.add_argument('filename', type=str, help='json file containing all players')
+    args = parser.parse_args(sys.argv[1:])
+    drawAll = vars(args)['all']
+    drawActives = vars(args)['actives']
+    fn = vars(args)['filename']
+    if os.path.exists(fn) and os.path.isfile(fn):
+        main(fn, drawAll, drawActives)
+    else:
+        print("File '{}' does not exist.".format(sys.argv[1]))
